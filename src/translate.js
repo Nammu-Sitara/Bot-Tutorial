5f5a626688b5a0fd0460cb6ffa43829aeb9d2349
Sebastian Frey <sfrey@axioconcept.com>
2025-01-28 15:41:55 +0100

    fixed language output in reply

--- src/translate.js
+++ src/translate.js
@@ -3,5 +3,5 @@ module.exports = async function translateInput(interaction, textToTranslate) {
 	const res = await fetch('https://translate.google.com/translate_a/single?client=gtx&sl=auto&tl=' + languageDestination + '&dt=t&q=' + textToTranslate);
 	const responseText = await res.text();
 	const responseJson = await JSON.parse(responseText);
-	interaction.reply(responseJson[0][0][0] + '\n\n' + responseJson[0][8] + ' to ' + responseJson[0][8]);
+	interaction.reply(responseJson[0][0][0] + '\n\n' + responseJson[2].toUpperCase() + ' to ' + languageDestination);
 }
