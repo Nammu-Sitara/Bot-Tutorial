38a155b2f305e8fac1af38311f57cb085409bbc3
Sebastian Frey <sfrey@axioconcept.com>
2025-01-28 17:17:57 +0100

    removed debug logs

--- src/translate.js
+++ src/translate.js
@@ -2,13 +2,10 @@ module.exports = async function translate(textToTranslate, targetLanguage) {
 	const res = await fetch(`https://translate.google.com/translate_a/single?client=gtx&sl=auto&tl=${targetLanguage}&dt=t&q=${textToTranslate}`);
 	const responseText = await res.text();
 	const responseJson = await JSON.parse(responseText);
-	console.log(responseJson);
-	console.log(responseJson.length);
 	const translation = [];
 	for (const element of responseJson[0]) {
 		translation.push(element[0]);
 	}
-	console.log(translation);
 	return {
 		'translation': translation.join(''),
 		'sourceLanguage': responseJson[responseJson.length - 7].toUpperCase(),
