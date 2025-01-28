d56ba80e3c2c467fd64e340ec19b622cc82b225e
Sebastian Frey <sfrey@axioconcept.com>
2025-01-28 15:51:18 +0100

    changed to use imported function now

--- commands/translations/translateInput.js
+++ commands/translations/translateInput.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const translate = require('../../src/translate.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -14,21 +15,8 @@ module.exports = {
 				.setRequired(false)),
 
 	async execute(interaction) {
-		await translateInput(interaction);
+		const input = interaction.options.getString('argument');
+
+		await translate(interaction, input);
 	},
 };
-
-async function translateInput(interaction) {
-	const input = interaction.options.getString('argument');
-	let languageDestination = interaction.options.getString('language');
-	if (languageDestination === null) {
-		languageDestination = 'EN';
-	}
-	const res = await fetch('https://translate.google.com/translate_a/single?client=gtx&sl=auto&tl=' + languageDestination + '&dt=t&q=' + input);
-	const responseText = await res.text();
-	const responseJson = await JSON.parse(responseText);
-	console.log(languageDestination);
-	// console.log(responseJson[0]);
-	// console.log('Translated: ' + responseJson[0][0][0] + '\n\n' + responseJson[0][8] + '->' + responseJson[0][8]);
-	interaction.reply(responseJson[0][0][0] + '\n\n' + responseJson[2].toUpperCase() + ' to ' + languageDestination);
-}
